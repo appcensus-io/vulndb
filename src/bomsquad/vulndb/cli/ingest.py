6faf936fbbb4029975b05e3fc3b468d2aa3cef4a
pakbaz-dev <68182910+pakbaz-dev@users.noreply.github.com>
2024-05-13 14:34:14 -0600

    Fix issue:   "vulndb nvd ingest --update" does not acquire new records, it only updates the existing records

--- src/bomsquad/vulndb/cli/ingest.py
+++ src/bomsquad/vulndb/cli/ingest.py
@@ -12,14 +12,19 @@ osv_app = typer.Typer(name="osv")
 @nvd_app.command(name="ingest")
 def _nvd_ingest(
     scope: Optional[str] = typer.Option(default=None, help="Ingest only cve or cpe "),
-    offset: int = typer.Option(default=0, help="Offset into available entries to begin wtih"),
     update: bool = typer.Option(default=False, help="Acquire records newer than current data"),
 ) -> None:
     db = NVDDB()
     if scope == "cve" or scope is None:
-        Ingest.cve(offset, last_mod_start_date=db.cve_last_modified() if update else None)
+        # Update the existing records
+        Ingest.cve(offset=0, last_mod_start_date=db.cve_last_modified() if update else None)
+        # Acquire new records
+        Ingest.cve(offset=db.cve_count() if update else 0)
     if scope == "cpe" or scope is None:
-        Ingest.cpe(offset, last_mod_start_date=db.cpe_last_modified() if update else None)
+        # Update the existing records
+        Ingest.cpe(offset=0, last_mod_start_date=db.cpe_last_modified() if update else None)
+        # Acquire new records
+        Ingest.cpe(offset=db.cpe_count() if update else 0)
 
 
 @osv_app.command(name="ingest")
